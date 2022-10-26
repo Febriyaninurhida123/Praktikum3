5c718280ae0de9c7edb250b5c260beebad896a09
Febriyaninurhida123 <sutarmiprihatna@gmail.com>
2022-10-26 23:24:18 +0700

    menambahkan file

---
+++ Menghitung_Luas_dan_Keliling_Lingkaran.py
@@ -0,0 +1,9 @@
+import math
+r = float(input("Masukan jari-jari : "))
+
+luas = math.pi*(r*r)
+keliling = 2*math.pi*r
+
+
+print ("Luas Lingkaran \t= ",format(luas,'.2f'))
+print ("Keliling Lingkaran \t= ",format(keliling,'.2f'))
